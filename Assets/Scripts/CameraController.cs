a53c1955f792b7f06c1f4aa0f1ff8e65fd685952
BouhS <sabblah6277@gmail.com>
2017-03-07 08:01:51 +0100

    Side camera added + Main Camera fixed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,20 +7,24 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
 
     private Vector3 offset;
-    private int rows,columns;
+    private float rows,columns,wallThick,floorWidth,floorHeight;
     // Use this for initialization
     void Start()
     {
+        rows = PlayerPrefs.GetInt("rows", 1) ;
+        columns = PlayerPrefs.GetInt("columns", 1);
+        wallThick = PlayerPrefs.GetFloat("wallThickness");
+        floorHeight = PlayerPrefs.GetFloat("floorHeight") ;
+        floorWidth= PlayerPrefs.GetFloat("floorWidth");
+        if(this.name == "Main Camera2")
+        {
+            this.transform.position = new Vector3(columns / 2, Mathf.Sqrt(Mathf.Pow(columns * floorWidth, 2) + Mathf.Pow(rows * floorHeight, 2)), -rows / 2);
 
-        rows = PlayerPrefs.GetInt("rows",1);
-        columns = PlayerPrefs.GetInt("columns",1);
 
-
-        Vector3 off = new Vector3(-rows / 2, 0, columns / 2);
-        Vector3 pos = new Vector3(columns / 2, 2 + Mathf.Sqrt(Mathf.Pow(columns*2/3, 2) + Mathf.Pow(rows *2/3, 2)), - rows / 2);
-        transform.position = pos + off;        
-        //offset = transform.position - pos;
-        //offset = transform.position - player.transform.position;
+        }else if (this.name == "SideView Camera")
+        {
+            this.transform.position = new Vector3(columns/2 , 0 , -rows*4 );
+        }
     }
 
     // Update is called once per frame
