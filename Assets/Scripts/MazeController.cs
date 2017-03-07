9ef19c9de792a0b834cbda0e23dc7e6cd3cae84f
BouhS <sabblah6277@gmail.com>
2017-03-07 08:03:42 +0100

    Maze tilting angles managed

--- Assets/Scripts/MazeController.cs
+++ Assets/Scripts/MazeController.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class MazeController : MonoBehaviour {
 
-    private float angleX, angleZ;
+    private float angleX, angleZ ,angleXMax,angleZMax ,tempX,tempZ;
 	// Use this for initialization
 	void Start ()
     {
-        transform.TransformPoint(PlayerPrefs.GetInt("rows")/ 2,0, -PlayerPrefs.GetInt("columns") / 2);
+        angleX = 0;
+        angleZ = 0;
+        angleXMax = 20;
+        angleZMax = 20;
+        
 
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        
-        angleX = transform.rotation.eulerAngles.x;
-        angleZ = transform.rotation.eulerAngles.z;
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
         {
-            if ((angleX >= 0 && angleX <= 45) || (angleX >= 315 && angleX <= 360) || (angleX < (45 + 1) && moveVertical < 0) || (angleX > (315 - 1) && moveVertical > 0))
-            {
-                transform.Rotate(new Vector3(moveVertical, 0, 0), Space.Self);
-                //     transform.RotateAround(new Vector3(MazeRows / 2, 0, -MazeColumns / 2), new Vector3(1, 0, 0), moveVertical);
-
+              if (Mathf.Abs(angleX + moveVertical) <= angleXMax)
+                {
+                transform.RotateAround(Vector3.zero, Vector3.right, moveVertical);
+                angleX += moveVertical;
             }
         }
         else
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
         {
-            if ((angleZ >= 0 && angleZ <= 45) || (angleZ >= 315 && angleZ <= 360) || (angleZ < (45 + 1) && moveHorizontal > 0) || (angleZ > (315 - 1) && moveHorizontal < 0))
-            {
-                transform.Rotate(new Vector3(0, 0, -moveHorizontal), Space.Self);
-                //transform.RotateAround(new Vector3(MazeRows / 2, 0, -MazeColumns / 2), new Vector3(0, 0, 1), -moveHorizontal);
-
+              if (Mathf.Abs(angleZ - moveHorizontal) <= angleZMax)
+                {
+                transform.RotateAround(Vector3.zero, Vector3.forward, -moveHorizontal);
+                angleZ += -moveHorizontal;
             }
         }
     }
--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -2,39 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGenerator : MonoBehaviour {
+public class MazeGenerator : MonoBehaviour
+{
 
-    public GameObject wall,floor,roof;
+    public GameObject wall, floor, roof;
     private int MazeRows, MazeColumns;
 
     private MazeCell[,] maze;
-    private Vector3 pos, vec ,scale , floorPos, roofPos;
+    private Vector3 pos, vec, scale, floorPos, roofPos;
     private Quaternion rot;
     private float angleX, angleZ;
+    private float width, height;
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        MazeRows = PlayerPrefs.GetInt("rows",1);
-        MazeColumns = PlayerPrefs.GetInt("columns",1);
 
-        Vector3 off = new Vector3(-MazeRows / 2, 0, MazeColumns / 2);
+        PlayerPrefs.SetFloat("wallThickness", wall.transform.localScale.z);
+        PlayerPrefs.SetFloat("floorWidth", floor.transform.localScale.x );
+        PlayerPrefs.SetFloat("floorHeight", floor.transform.localScale.z );
+
+        MazeRows = PlayerPrefs.GetInt("rows", 1);
+        MazeColumns = PlayerPrefs.GetInt("columns", 1);
+
+        Vector3 off = new Vector3(-MazeColumns / 2, 0, MazeRows / 2);
         pos = wall.transform.position + off;
         scale = wall.transform.localScale;
         floorPos = floor.transform.position + off;
         roofPos = roof.transform.position + off;
+        width = floor.transform.localScale.x;
+        height = floor.transform.localScale.z;
         InitMaze();
         KruskalAlgo();
 
     }
 
-    
+
 
     //Initalize the Maze
     void InitMaze()
     {
-        maze = new MazeCell[MazeRows,MazeColumns];
+        maze = new MazeCell[MazeRows, MazeColumns];
 
         for (int r = 0; r < MazeRows; r++)
         {
@@ -44,33 +53,35 @@ public class MazeGenerator : MonoBehaviour {
                 if (r == 0)
                 {
                     //create NorthWall
-                    vec.Set(pos.x + (c), pos.y, pos.z);
+                    vec.Set(pos.x + (c*width), pos.y, pos.z);
                     rot = Quaternion.Euler(0, 0, 0);
-                   
-                    mazeCell.NWall= Instantiate(wall, vec, rot);
+
+                    mazeCell.NWall = Instantiate(wall, vec, rot);
                     mazeCell.NWall.transform.parent = this.transform;
                     mazeCell.NWall.name = "North Wall " + r + " " + c + "";
 
-                }else{
-                    mazeCell.NWall = maze[r-1,c].SWall;
+                }
+                else
+                {
+                    mazeCell.NWall = maze[r - 1, c].SWall;
 
                 }
 
                 //create Floor
-                vec.Set(floorPos.x + (c), floorPos.y, floorPos.z - r);
+                vec.Set(floorPos.x + (c * width), floorPos.y, floorPos.z - (r*height));
                 rot = Quaternion.Euler(0, 0, 0);
                 mazeCell.Floor = Instantiate(floor, vec, rot);
                 mazeCell.Floor.transform.parent = this.transform;
                 mazeCell.Floor.name = "Floor " + r + " " + c + "";
 
                 //create Roof
-                vec.Set(roofPos.x + (c), roofPos.y, roofPos.z - r);
+                vec.Set(roofPos.x + (c * width), roofPos.y, roofPos.z - r*height);
                 mazeCell.Roof = Instantiate(roof, vec, rot);
                 mazeCell.Roof.transform.parent = this.transform;
                 mazeCell.Roof.name = "Roof " + r + " " + c + "";
 
                 //create SouthWall
-                vec.Set(pos.x + (c), pos.y, pos.z - (r) - scale.x );
+                vec.Set(pos.x + (c * width), pos.y, pos.z - (r*height) - scale.x);
                 rot = Quaternion.Euler(0, 0, 0);
                 mazeCell.SWall = Instantiate(wall, vec, rot);
                 mazeCell.SWall.transform.parent = this.transform;
@@ -79,18 +90,20 @@ public class MazeGenerator : MonoBehaviour {
                 if (c == 0)
                 {
                     //create WestWall
-                    vec.Set(pos.x + (c) - (scale.x / 2), pos.y, pos.z - (r) - (scale.x / 2));
-                    rot = Quaternion.Euler(0,90, 0);
+                    vec.Set(pos.x + (c * width) - (scale.x / 2), pos.y, pos.z - (r*height) - (scale.x / 2));
+                    rot = Quaternion.Euler(0, 90, 0);
                     mazeCell.WWall = Instantiate(wall, vec, rot);
                     mazeCell.WWall.transform.parent = this.transform;
                     mazeCell.WWall.name = "West Wall " + r + " " + c + "";
-                }else{
-                    mazeCell.WWall = maze[r , c-1].EWall;
+                }
+                else
+                {
+                    mazeCell.WWall = maze[r, c - 1].EWall;
 
                 }
                 //create EastWall
-                
-                vec.Set((pos.x + (c) + (scale.x) - (scale.x / 2)), pos.y, pos.z - (r) - (scale.x / 2));
+
+                vec.Set((pos.x + (c *width) + (scale.x) - (scale.x / 2)), pos.y, pos.z - (r*height) - (scale.x / 2));
                 rot = Quaternion.Euler(0, 90, 0);
                 mazeCell.EWall = Instantiate(wall, vec, rot);
                 mazeCell.EWall.transform.parent = this.transform;
@@ -98,11 +111,11 @@ public class MazeGenerator : MonoBehaviour {
 
                 mazeCell.Val = MazeColumns * r + c;
 
-                maze[r,c] = mazeCell;
+                maze[r, c] = mazeCell;
             }
         }
         maze[MazeRows - 1, MazeColumns - 1].Floor.tag = "goal";
-        maze[MazeRows - 1, MazeColumns - 1].Floor.GetComponent<Renderer>().material.color = new Color(1,0,0);
+        maze[MazeRows - 1, MazeColumns - 1].Floor.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
 
     }
 
@@ -111,52 +124,26 @@ public class MazeGenerator : MonoBehaviour {
         return ((r == 0 && w == 0) || (r == MazeRows - 1 && w == 1) || (c == 0 && w == 2) || (c == MazeColumns - 1 && w == 3));
 
     }
-    
-    bool isClosedCell(int r,int c)
+
+    bool isClosedCell(int r, int c)
     {
-        return (( maze[r, c].NWall != null) && (maze[r, c].SWall != null) && (maze[r, c].EWall != null) && (maze[r, c].WWall != null) );
+        return ((maze[r, c].NWall != null) && (maze[r, c].SWall != null) && (maze[r, c].EWall != null) && (maze[r, c].WWall != null));
     }
     //r : row , c : column , w : way, val : value
-    void spreadValue(int r, int c, int w,int val)
+    void spreadValue(int r, int c, int w, int val)
     {
         maze[r, c].Val = val;
         switch (w)
         {
             case 0://North
-                if(r > 0 && maze[r,c].NWall == null )//pas de mur au nord
+                if (r > 0 && maze[r, c].NWall == null)//pas de mur au nord
                 {
-                    if(maze[r-1,c].Val != val)
+                    if (maze[r - 1, c].Val != val)
                     {
-                        spreadValue(r-1,c,0,val);
+                        spreadValue(r - 1, c, 0, val);
                     }
                 }
-                if (c < MazeColumns-1 && maze[r , c].EWall == null)//pas de mur a l'est
-                {
-                    if (maze[r, c+1].Val != val)
-                    {
-                        spreadValue(r, c+1, 2, val);
-                    }
-
-                }
-                if (c>0 && maze[r , c].WWall == null)//pas de mur a gauche
-                {
-                    if (maze[r, c-1].Val != val)
-                    {
-                        spreadValue(r, c-1, 3, val);
-                    }
-
-                }
-
-                break;
-            case 1://South
-                if (r < MazeRows-1 && maze[r, c].SWall == null)//pas de mur au south
-                {
-                    if (maze[r + 1, c].Val != val)
-                    {
-                        spreadValue(r + 1, c, 1, val);
-                    }
-                }
-                if (c < MazeColumns-1 && maze[r, c].EWall == null)//pas de mur a l'est
+                if (c < MazeColumns - 1 && maze[r, c].EWall == null)//pas de mur a l'est
                 {
                     if (maze[r, c + 1].Val != val)
                     {
@@ -172,7 +159,33 @@ public class MazeGenerator : MonoBehaviour {
                     }
 
                 }
-                
+
+                break;
+            case 1://South
+                if (r < MazeRows - 1 && maze[r, c].SWall == null)//pas de mur au south
+                {
+                    if (maze[r + 1, c].Val != val)
+                    {
+                        spreadValue(r + 1, c, 1, val);
+                    }
+                }
+                if (c < MazeColumns - 1 && maze[r, c].EWall == null)//pas de mur a l'est
+                {
+                    if (maze[r, c + 1].Val != val)
+                    {
+                        spreadValue(r, c + 1, 2, val);
+                    }
+
+                }
+                if (c > 0 && maze[r, c].WWall == null)//pas de mur a gauche
+                {
+                    if (maze[r, c - 1].Val != val)
+                    {
+                        spreadValue(r, c - 1, 3, val);
+                    }
+
+                }
+
                 break;
             case 2://East
 
@@ -184,14 +197,14 @@ public class MazeGenerator : MonoBehaviour {
                     }
                 }
 
-                if (r < MazeRows-1 && maze[r, c].SWall == null)//pas de mur au south
+                if (r < MazeRows - 1 && maze[r, c].SWall == null)//pas de mur au south
                 {
                     if (maze[r + 1, c].Val != val)
                     {
                         spreadValue(r + 1, c, 1, val);
                     }
                 }
-                if (c < MazeColumns-1 && maze[r, c].EWall == null)//pas de mur a l'est
+                if (c < MazeColumns - 1 && maze[r, c].EWall == null)//pas de mur a l'est
                 {
                     if (maze[r, c + 1].Val != val)
                     {
@@ -209,7 +222,7 @@ public class MazeGenerator : MonoBehaviour {
                     }
                 }
 
-                if (r < MazeRows-1 && maze[r, c].SWall == null)//pas de mur au south
+                if (r < MazeRows - 1 && maze[r, c].SWall == null)//pas de mur au south
                 {
                     if (maze[r + 1, c].Val != val)
                     {
@@ -232,10 +245,10 @@ public class MazeGenerator : MonoBehaviour {
 
     void KruskalAlgo()
     {
-        int rRand, cRand,wRand;
+        int rRand, cRand, wRand;
         int groupNb = MazeColumns * MazeRows;
         int count = 1;
-        int MAX = (MazeColumns * MazeRows )*10;
+        int MAX = (MazeColumns * MazeRows) * 10;
         while (groupNb > 1 && count < MAX)
         {
             //Randomly picking a wall
@@ -250,7 +263,7 @@ public class MazeGenerator : MonoBehaviour {
                 {
                     case 0:
                         //IF wall isn't destroyed AND values of the cells differ
-                        if (maze[rRand, cRand].NWall != null && maze[rRand,cRand].Val != maze[rRand - 1, cRand].Val)
+                        if (maze[rRand, cRand].NWall != null && maze[rRand, cRand].Val != maze[rRand - 1, cRand].Val)
                         {
                             //Destroy the wall
                             Destroy(maze[rRand, cRand].NWall);
@@ -273,7 +286,7 @@ public class MazeGenerator : MonoBehaviour {
 
                         break;
                     case 2:
-                        if (maze[rRand, cRand].EWall != null && maze[rRand, cRand].Val != maze[rRand , cRand+1].Val)
+                        if (maze[rRand, cRand].EWall != null && maze[rRand, cRand].Val != maze[rRand, cRand + 1].Val)
                         {
                             Destroy(maze[rRand, cRand].EWall);
                             maze[rRand, cRand].EWall = null;
@@ -283,7 +296,7 @@ public class MazeGenerator : MonoBehaviour {
                         }
                         break;
                     case 3:
-                        if (maze[rRand, cRand].WWall != null && maze[rRand, cRand].Val != maze[rRand , cRand-1].Val)
+                        if (maze[rRand, cRand].WWall != null && maze[rRand, cRand].Val != maze[rRand, cRand - 1].Val)
                         {
                             Destroy(maze[rRand, cRand].WWall);
                             maze[rRand, cRand].WWall = null;
@@ -300,19 +313,19 @@ public class MazeGenerator : MonoBehaviour {
         }
         if (groupNb > 1)
         {
-            for(int i = 0; i < MazeRows; i++)
+            for (int i = 0; i < MazeRows; i++)
             {
-                for(int j = 0; j < MazeColumns; j++)
+                for (int j = 0; j < MazeColumns; j++)
                 {
-                    if ( i>0 && maze[i, j].NWall != null && maze[i, j].Val != maze[i - 1, j].Val)
+                    if (i > 0 && maze[i, j].NWall != null && maze[i, j].Val != maze[i - 1, j].Val)
                     {
                         Destroy(maze[i, j].NWall);
                         maze[i, j].NWall = null;
-                        maze[i - 1,j].SWall = null;
+                        maze[i - 1, j].SWall = null;
                         //On propage la valeur
                         spreadValue(i - 1, j, 0, maze[i, j].Val);
                     }
-                    if(i<MazeRows-1 && maze[i, j].SWall != null && maze[i, j].Val != maze[i + 1, j].Val)
+                    if (i < MazeRows - 1 && maze[i, j].SWall != null && maze[i, j].Val != maze[i + 1, j].Val)
                     {
 
                         Destroy(maze[i, j].SWall);
@@ -321,22 +334,22 @@ public class MazeGenerator : MonoBehaviour {
                         //On propage la valeur
                         spreadValue(i + 1, j, 1, maze[i, j].Val);
                     }
-                    if (j < MazeColumns - 1 && maze[i, j].EWall != null && maze[i, j].Val != maze[i , j+1].Val)
+                    if (j < MazeColumns - 1 && maze[i, j].EWall != null && maze[i, j].Val != maze[i, j + 1].Val)
                     {
                         Destroy(maze[i, j].EWall);
                         maze[i, j].EWall = null;
-                        maze[i , j+1].WWall = null;
+                        maze[i, j + 1].WWall = null;
                         //On propage la valeur
-                        spreadValue(i , j+1, 2, maze[i, j].Val);
+                        spreadValue(i, j + 1, 2, maze[i, j].Val);
                     }
-                    if (j > 0 && maze[i, j].WWall != null && maze[i, j].Val != maze[i , j-1].Val)
+                    if (j > 0 && maze[i, j].WWall != null && maze[i, j].Val != maze[i, j - 1].Val)
                     {
 
                         Destroy(maze[i, j].WWall);
                         maze[i, j].WWall = null;
-                        maze[i , j-1].EWall = null;
+                        maze[i, j - 1].EWall = null;
                         //On propage la valeur
-                        spreadValue(i , j-1, 3, maze[i, j].Val);
+                        spreadValue(i, j - 1, 3, maze[i, j].Val);
                     }
 
                 }
