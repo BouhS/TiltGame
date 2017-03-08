ae43c673e574e52f8f12723241f7c25517007a35
BouhS <sabblah6277@gmail.com>
2017-03-08 20:58:29 +0100

    Movement fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,21 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+       rb = GetComponent<Rigidbody>();
         winMessage.text = "";
         int rows = PlayerPrefs.GetInt("rows", 1);
         int columns = PlayerPrefs.GetInt("columns", 1);
         transform.position += new Vector3(-columns / 2, 0, rows / 2);
     }
-    
+
+   private void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement);
+    }
     void OnCollisionEnter(Collision other)
     {        
         if (other.gameObject.CompareTag("goal") )
